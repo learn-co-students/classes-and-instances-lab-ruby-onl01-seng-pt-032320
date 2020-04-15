e3232b5bf1a59dd8b543b5b5c7e9e0261eac601c
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-15 05:44:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+# Dog
+#   is defined within lib/dog.rb
+#   variables defined in lib/dog.rb
+#     defines a local variable fido as an instance of a Dog
+#     defines a local variable snoopy as an instance of a Dog
+#     defines a local variable lassie as an instance of a Dog
+class Dog
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
